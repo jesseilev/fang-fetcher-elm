740dcae64ea6979a98ee14fd0b3b5edae304eefc
jesseilev <jesseilev@gmail.com>
2019-03-06 00:39:46 -0500

    vampire styling

--- src/Main.elm
+++ src/Main.elm
@@ -5,9 +5,13 @@ import Html as Html exposing (Html)
 import Element as El exposing (Element)
 import Element.Input as Input
 import Element.Events as Events
+import Element.Background as Background
+import Element.Border as Border
+import Element.Font as Font
 import Http
 import Json.Decode as Json exposing (Decoder)
 import Dict exposing (Dict)
+import NumberSuffix as Number
 
 
 main : Program () Model Msg
@@ -18,6 +22,10 @@ main =
         , update = update
         , view = view
         }
+
+
+
+-- MODEL
 
 
 type alias Model =
@@ -42,7 +50,7 @@ type Repos
 type alias Repo =
     { repoName : String
     , htmlUrl : String
-    , stargazersCount : Int
+    , stars : Int
     , description : Maybe String
     }
 
@@ -111,7 +119,7 @@ update msg model =
         GotRepos githubOrgName result ->
             let
                 top5ReposResult =
-                    Result.map (List.take 5) result
+                    Result.map (List.take 30) result
                         |> Debug.log "result"
 
                 updateReposInCompany _ company =
@@ -125,88 +133,6 @@ update msg model =
 
         _ ->
             model |> withNoCmds
-
-
-
--- VIEW
-
-
-view : Model -> Html Msg
-view model =
-    El.layout []
-        <| El.column []
-            [ El.el [] <| El.text "FANG Fetcher"
-            , viewSelector model
-            , Maybe.map viewCompany
-                (Dict.get model.selectedCompany model.companies)
-                |> Maybe.withDefault El.none
-            ]
-
-
-viewSelector : Model -> Element Msg
-viewSelector model =
-    let
-        viewCompanyOption c =
-            viewOption c.companyName
-                c.companyName
-                (c.companyName == model.selectedCompany)
-    in
-        El.row []
-            <| List.map viewCompanyOption (Dict.values model.companies)
-
-
-viewOption : String -> String -> Bool -> Element Msg
-viewOption key title isSelected =
-    El.el [ Events.onClick <| SelectCompany key ]
-        (El.text
-            <| if isSelected then
-                String.toUpper title
-               else
-                title
-        )
-
-
-viewCompany : Company -> Element Msg
-viewCompany company =
-    El.column []
-        [ viewRepos company
-        ]
-
-
-viewRepos : Company -> Element Msg
-viewRepos company =
-    let
-        viewRepo repo =
-            El.el [] <| El.text repo.repoName
-
-        viewNotLoaded maybeError =
-            El.column []
-                [ El.text <| Maybe.withDefault "" maybeError
-                , Input.button []
-                    { onPress = Just <| RequestRepos company.githubOrgName
-                    , label = El.text <| "Load repos from " ++ company.companyName
-                    }
-                ]
-
-        viewLoading =
-            El.text "Loading ..."
-    in
-        El.el []
-            <| case company.repos of
-                Loaded result ->
-                    case result of
-                        Ok repos ->
-                            El.column []
-                                <| List.map viewRepo repos
-
-                        Err error ->
-                            viewNotLoaded (Just "Hmm, something went wrong. Try again?")
-
-                NotLoaded ->
-                    viewNotLoaded Nothing
-
-                Loading ->
-                    viewLoading
 
 
 
@@ -234,17 +160,322 @@ githubUrl githubOrgName =
         ++ githubOrgName
 
 
-httpErrorToString error =
-    case error of
-        Http.BadBody str ->
-            "BadBody: " ++ str
 
-        _ ->
-            "Error!"
+-- VIEW
+
+
+colorPalette =
+    { darkGrey = El.rgb 0.35 0.35 0.35
+    , darkPurple = El.rgb 0.17 0.15 0.18
+    , darkestGrey = El.rgb 0.1 0.1 0.1
+    , white = El.rgb 1 1 1
+    , lightGrey = El.rgb 0.9 0.9 0.9
+    , lightestGrey = El.rgb 0.95 0.95 0.95
+    , grey = El.rgb 0.4 0.4 0.4
+    , red = El.rgb 0.99 0.25 0.1
+    , clear = El.rgba 0 0 0 0
+    }
+
+
+colors =
+    { optionSelected = colorPalette.darkPurple
+    , optionUnselected = colorPalette.darkPurple
+    , color1 =
+        colorPalette.lightGrey
+        --, color2 = colorPalette.darkPurple
+    }
+
+
+layout =
+    { paddingSmall = 8
+    , paddingLarge = 16
+    }
+
+
+fontScale =
+    round << El.modular 16 1.25
+
+
+paddingScale =
+    round << El.modular 12 1.5
+
+
+view : Model -> Html Msg
+view model =
+    El.layoutWith
+        { options =
+            [ El.focusStyle
+                { borderColor = Nothing
+                , backgroundColor = Nothing
+                , shadow = Nothing
+                }
+            ]
+        }
+        [ El.height El.fill
+        , Font.size <| fontScale 1
+        , Font.color colorPalette.darkestGrey
+        , Background.color colorPalette.darkPurple
+        , El.scrollbarY
+        ]
+        <| El.column
+            [ El.width El.fill
+            , El.height El.fill
+            , El.spacing <| paddingScale 4
+            ]
+            [ viewHeader
+            , viewMain model
+            , viewFooter
+            ]
+
+
+viewHeader : Element Msg
+viewHeader =
+    El.column
+        [ El.paddingXY (paddingScale 3) (paddingScale 3)
+        , El.width <| El.fill
+        , Background.color <| colorPalette.darkestGrey
+        , El.spacing 8
+        , Border.widthXY 0 3
+        , Border.color colorPalette.darkPurple
+        ]
+        [ El.el
+            [ Font.size <| fontScale 3
+            , Font.color colorPalette.grey
+            , El.centerX
+            , El.centerY
+            ]
+            <| El.text "FANG Fetcher"
+        , El.paragraph
+            [ Font.size <| fontScale -1
+            , Font.light
+            , Font.italic
+            , Font.color colorPalette.grey
+            , Font.center
+            , El.centerX
+            , El.centerY
+            ]
+            [ El.text
+                <| "Sink your teeth into the repositories "
+                ++ "of tech's most eternally young-blooded companies."
+            ]
+        ]
+
+
+viewFooter : Element Msg
+viewFooter =
+    El.row
+        [ El.width El.fill
+        , El.padding <| paddingScale 1
+        , El.alignBottom
+        , Background.color colorPalette.darkestGrey
+        , Font.color colorPalette.grey
+        , Font.size <| fontScale -1
+        ]
+        [ El.link []
+            { label = El.text " Github"
+            , url = "https://github.com/jesseilev/fang-fetcher-elm"
+            }
+        ]
+
+
+viewMain : Model -> Element Msg
+viewMain model =
+    El.column
+        [ El.centerX
+        , El.width (El.fill |> El.minimum 300 |> El.maximum 750)
+        , El.height El.fill
+        , Border.color <| colorPalette.darkestGrey
+        , Border.width 3
+        ]
+        [ viewSelector model
+        , Maybe.map viewCompany
+            (Dict.get model.selectedCompany model.companies)
+            |> Maybe.withDefault El.none
+        ]
+
+
+viewSelector : Model -> Element Msg
+viewSelector model =
+    let
+        viewCompanyOption c =
+            viewOption c.companyName
+                c.companyName
+                (c.companyName == model.selectedCompany)
+    in
+        El.row
+            [ El.width El.fill
+            , El.spaceEvenly
+            , El.pointer
+            ]
+            <| List.map viewCompanyOption (Dict.values model.companies)
+
+
+viewOption : String -> String -> Bool -> Element Msg
+viewOption key title isSelected =
+    El.el
+        [ Events.onClick <| SelectCompany key
+        , El.padding <| paddingScale 1
+        , El.width <| El.fillPortion 1
+        , Background.color
+            <| if isSelected then
+                colorPalette.darkPurple
+               else
+                colorPalette.darkestGrey
+        , Border.color colorPalette.darkestGrey
+        , Font.color colorPalette.grey
+        , El.mouseOver [ Font.color colorPalette.lightGrey ]
+        ]
+        <| El.text title
+
+
+viewCompany : Company -> Element Msg
+viewCompany company =
+    El.column
+        [ El.width El.fill
+        , El.height El.fill
+        , Background.color colors.optionSelected
+        , El.height El.fill
+        ]
+        [ viewRepos company
+        ]
+
+
+viewRepos : Company -> Element Msg
+viewRepos company =
+    let
+        shadowWithBlur blur =
+            { offset = ( 1, 1 )
+            , size = 0
+            , blur = blur
+            , color = colorPalette.darkestGrey
+            }
+
+        viewNotLoaded maybeError =
+            El.column
+                [ El.centerX
+                , El.centerY
+                , El.width El.fill
+                , El.height El.fill
+                ]
+                [ El.text <| Maybe.withDefault "" maybeError
+                , Input.button
+                    [ El.centerX
+                    , El.centerY
+                    , El.padding <| paddingScale 4
+                    , Font.color colorPalette.grey
+                    , Border.color colorPalette.clear
+                    , Border.rounded 2
+                    , Border.width 0
+                    , Border.shadow <| shadowWithBlur 1
+                    , El.mouseOver
+                        [ Font.color colorPalette.red
+                        , Border.color <| colorPalette.red
+                        , Border.shadow <| shadowWithBlur 8
+                        ]
+                    ]
+                    { onPress = Just <| RequestRepos company.githubOrgName
+                    , label =
+                        El.el [ El.centerX ]
+                            <| El.text ("Load repos from " ++ company.companyName)
+                    }
+                ]
+
+        viewLoading =
+            El.el
+                [ El.centerX
+                , El.centerY
+                , Font.color colorPalette.grey
+                ]
+                <| El.text "One moment please ..."
+    in
+        El.el
+            [ El.width El.fill
+            , El.height El.fill
+            ]
+            <| case company.repos of
+                Loaded result ->
+                    case result of
+                        Ok repos ->
+                            El.wrappedRow
+                                [ El.spaceEvenly
+                                , El.spacing <| paddingScale 3
+                                , El.padding <| paddingScale 4
+                                ]
+                                <| List.map viewRepo repos
+
+                        Err error ->
+                            viewNotLoaded (Just "Hmm, something went wrong. Try again?")
+
+                NotLoaded ->
+                    viewNotLoaded Nothing
+
+                Loading ->
+                    viewLoading
+
+
+viewRepo : Repo -> Element Msg
+viewRepo repo =
+    let
+        viewDescription description =
+            El.paragraph
+                [ Font.size <| fontScale -1
+                , Font.light
+                , Font.color colorPalette.grey
+                ]
+                [ El.text description ]
+
+        shadowWithBlur blur =
+            { offset = ( 1, 1 )
+            , size = 0
+            , blur = blur
+            , color = colorPalette.darkestGrey
+            }
+    in
+        El.column
+            [ El.width (El.fill |> El.minimum 200 |> El.maximum 400)
+            , El.spacing <| paddingScale 1
+            , El.padding <| paddingScale 3
+            , El.clipX
+            , El.scrollbarX
+            , Background.color colorPalette.darkPurple
+            , Border.color colorPalette.darkestGrey
+            , Border.width 0
+            , Border.rounded 2
+            , Border.shadow <| shadowWithBlur 1
+            , El.mouseOver [ Border.shadow <| shadowWithBlur 8 ]
+            ]
+            [ El.link
+                [ Font.color colorPalette.red
+                , Font.light
+                , El.mouseOver [ Font.color colorPalette.white ]
+                ]
+                { label = El.text repo.repoName
+                , url = repo.htmlUrl
+                }
+            , El.el
+                [ Font.color colorPalette.darkestGrey
+                , Font.size <| fontScale -1
+                ]
+                <| El.text ("★ " ++ prettyInt repo.stars)
+            , Maybe.map viewDescription repo.description
+                |> Maybe.withDefault El.none
+            ]
 
 
 
 -- UTIL
+
+
+prettyInt : Int -> String
+prettyInt =
+    let
+        standardConfig =
+            Number.standardConfig
+
+        config =
+            { standardConfig | getSuffix = Number.suffixStandardShort }
+    in
+        Number.format config << toFloat
 
 
 withNoCmds : a -> ( a, Cmd msg )
